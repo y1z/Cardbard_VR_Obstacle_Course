26e2eb44ffb8d6eb77bf2192bbf61d4e1febdcb6
y1z <whyis2cool@gmail.com>
2024-05-05 22:27:24 -0600

    Add script to Detect if Player Is In Range

---
+++ Assets/[Scripts/DetectIfPlayerIsInRange.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectIfPlayerIsInRange : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerReference = null;
+
+    [Tooltip("How close the player has to be to activate this")]
+    public float minimumRange;
+
+    public bool isInRange = false;
+    
+    void Start()
+    {
+        playerReference = GameObject.FindGameObjectWithTag("Player");
+        Debug.Assert(playerReference != null, "Add reference to player for script to work", this);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentDistanceFromPlayer = Vector3.Distance(playerReference.transform.position, transform.position);
+        if (!(currentDistanceFromPlayer < minimumRange))
+            return;
+
+        isInRange = true;
+    }
+}
---
+++ Assets/[Scripts/PlayerType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+/// <summary>
+/// This is just to add typing to the player character for purposes of searching
+/// </summary>
+public class PlayerType : MonoBehaviour
+{
+}
